938c36e1940d8b88d1c8a5714b3e7151ad767df6
kiricon <kiricon@live.com>
2016-09-02 21:13:43 -0600

    Added basic logic that doesn't entirly work yet. But it's a good frame. Frankie is forcing me to get off...

--- js/Search/SearchBar.js
+++ js/Search/SearchBar.js
@@ -1,11 +1,47 @@
 class SearchBar {
-	constructor(element){
-		this.element = element;
-	}
-	AnalyzeInput(){
-		var input = this.element.value;
-		
+	//Build out our searchbar and pre define properties
+	constructor(){
+		//this.element = document.getElementById('search');
+		this.results = [];
+		this.options = [
+			{text: "hotdog"},
+			{text: "hotdong"},
+			{text: "wutang"}
+		];
+		this.resultsElement = document.getElementById('results');
 	}
 
-	this.element.addEventListener('keyup', this.AnalyzeInput);
+	Listen(){
+		document.getElementById('search').addEventListener('change', this.AnalyzeInput(this));
+	}
+
+	AnalyzeInput(context){
+		console.log('y');
+		context.results = [];
+		var input = document.getElementById('search').value;
+		if(input != ""){
+		input = input.split(' ');
+		console.log(context);
+		context.options.forEach(function(value){
+			input.forEach(function(string){
+				if(value.text.indexOf(string) >= 0){
+					console.log('hellooo');
+					context.results.push(value);
+				}
+			});
+			
+		});
+		context.Update();
+		}
+	}
+
+	Update(){
+		var resultList = "";
+		this.results.forEach(function(value){
+			resultList += '<div class="result">'+value.text+'</div>';
+		});
+		this.resultsElement.innerHTML = resultList;
+	}
+
+	
 }
---
+++ js/app.js
@@ -0,0 +1,12 @@
+var search = new SearchBar();
+document.getElementById('search').addEventListener('keyup', search.AnalyzeInput(search));
+const remote = require("electron").remote;
+    var win = new remote.getCurrentWindow();
+    document.addEventListener("keydown", event => {
+
+        switch (event.key) {
+            case "Escape":
+                win.hide();
+                break;
+             }
+    });
