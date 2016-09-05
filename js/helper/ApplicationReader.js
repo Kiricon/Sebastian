f3b0e98f71b8131c5516855b1c428b3a70dfa5bb
kiricon <kiricon@live.com>
2016-09-04 23:52:09 -0600

    Did a reformat, and got ALL applications and icons reading in to the list!

--- js/helper/ApplicationReader.js
+++ js/helper/ApplicationReader.js
@@ -1,121 +1,106 @@
 const fs = require('fs');
 const plist = require('plist');
 const iconutil = require('iconutil');
-const path = require('path'); 
+const path = require('path');
 
-exports.getApplicationsList = function(callback){
-		if(process.platform == 'darwin'){
-	var fileList = [];
-	fs.readdir('/Applications', function(err, files){
-		var i = 1;
-		files.forEach(function(value){
-			
-			value = value.replace(" ", "\ ");
-			if(value.indexOf(".app") > -1 && value[0] != "."){
-				getIcon(value, function(encoding){
-					var obj = {
-						text: value.replace(".app", ""),
-						icon: encoding
-					}
-					fileList.push(obj);
-					i++;
-					if(i == files.length){
-						//console.log(fileList);
-						callback(fileList);
-					}
-				})
-			}else if(value[0] != "."){
-				fs.readdir('/Applications/'+value, function(err, files){
-					files.forEach(function(value){
 
-					})
-				});
-				fileList.push({text: value, icon: null});
-				i++;
-				if(i == files.length){
-					//console.log(fileList);
-					callback(fileList);
-				}
-			}else{
-				i++;
-				if(i == files.length){
-					console.log(fileList);
-					callback(fileList);
-				}
-			}
-		});
-		//console.log(fileList);
-		
-	});
+
+exports.getApplicationsList = function(callback) {
+        var fileList = [];
+        var i = 1;
+        getPaths(function(paths, names) {
+            paths.forEach(function(path, index) {
+                getIcon(path, function(encoding) {
+                    var obj = {
+                        text: names[index].replace(".app", ""),
+                        icon: encoding
+                    }
+                    i++;
+                    console.log(i + " : " + paths.length);
+                    fileList.push(obj);
+                    if (i == paths.length) {
+                        callback(fileList);
+                    }
+                });
+            });
+        });
+    }
+
+function getPaths(callback) {
+    var filePaths = [];
+    var fileNames = [];
+    var i = 1;
+
+    var done = false;
+    fs.readdir('/Applications', function(err, files) {
+        var z = files.length;
+        files.forEach(function(value) {
+            if (value.indexOf(".app") > -1) {
+                filePaths.push("/Applications/" + value);
+                fileNames.push(value);
+                i++;
+                
+			if(i == z && !done){
+				done = true;
+				 callback(filePaths, fileNames);
+			} 
+            } else if (value.indexOf('.') == -1) {
+                var apps = fs.readdirSync('/Applications/' + value);
+                    z += apps.length;
+                    apps.forEach(function(app) {
+                        if (app.indexOf(".app") > -1) {
+                            filePaths.push("/Applications/" + value + '/' + app);
+                            fileNames.push(app);
+                        }
+                        i++;
+                        if (i == z && !done) {
+                            done = true;
+                            callback(filePaths, fileNames);
+                        }
+                    });
+                    i++;
+                    if (i == z && !done) {
+                        done = true;
+                        callback(filePaths, fileNames);
+                    }
+
+            } else {
+                i++;
+                if (i == z && !done) {
+                    done = true;
+                    callback(filePaths, fileNames);
+                }
+            }
+        });
+    });
 }
+/*
+getPaths(function(paths, names){
+	console.log(paths);
+}) */
 
-} 
-
-function getPaths(){
-	var filePaths = [];
-	var i = 1;
-	var z = 0;
-	fs.readdir('/Applications', function(err, files){
-		z += files.length;
-		console.log(z);
-		files.forEach(function(value){
-			if(value.indexOf(".app") > -1){
-			filePaths.push("/Applications/"+value);
-			i++;
-			if(i == z){
-				console.log(filePaths);
-			}
-		}else if(value.indexOf('.') == -1){
-			fs.readdir('/Applications/'+value, function(err, apps){
-				z+= apps.length;
-				apps.forEach(function(app){
-					if(app.indexOf(".app") > -1){
-						filePaths.push("/Applications/"+value+'/'+app);
-					}
-					i++;
-						if(i == z){
-							console.log(filePaths);
-						}
-				});
-			});
-			i++;
-						if(i == z){
-							console.log(filePaths);
-						}
-		}else{
-			i++;
-						if(i == z){
-							console.log(filePaths);
-						}
-		}
-		});
-	});
-}
-
-getPaths();
-
-var getIcon = function(app, callback){
-
-	var file = "/Applications/"+app+"/Contents/Info.plist";
-	try {
-	  var obj = plist.parse(fs.readFileSync(file, 'utf8'));
-	  	if(obj.CFBundleIconFile.indexOf('.icns') > -1){
-		var iconPath = "/Applications/"+app+"/Contents/Resources/"+obj.CFBundleIconFile;
-		}else{
-			var iconPath = "/Applications/"+app+"/Contents/Resources/"+obj.CFBundleIconFile+".icns";
-		}
-		iconutil.toIconset(iconPath, function(err, icons) { 
-	   try {
-	    callback(icons['icon_128x128@2x.png'].toString('base64'));
-		}catch (e){
-			callback(null);
-		}
-		});
-	} catch (e) {
-	  callback(null);
-	}
-}
-
+var getIcon = function(app, callback) {
+        console.log(app);
+        var file = app + "/Contents/Info.plist";
+        try {
+            var obj = plist.parse(fs.readFileSync(file, 'utf8'));
+            if (obj.CFBundleIconFile.indexOf('.icns') > -1) {
+                var iconPath = app + "/Contents/Resources/" + obj.CFBundleIconFile;
+            } else {
+                var iconPath = app + "/Contents/Resources/" + obj.CFBundleIconFile + ".icns";
+            }
+            iconutil.toIconset(iconPath, function(err, icons) {
+                try {
+                    callback(icons['icon_128x128@2x.png'].toString('base64'));
+                } catch (e) {
+                    callback(null);
+                }
+            });
+        } catch (e) {
+            callback(null);
+        }
+    }
+    /*
 
 exports.getIcon = function(app, callback){
 
@@ -137,7 +122,7 @@ exports.getIcon = function(app, callback){
 	} catch (e) {
 	  callback(null);
 	}
-}
+} */
 
 /*
 getIcon("Atom.app", function(string){
