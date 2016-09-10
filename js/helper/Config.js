2c40108f31130ee07aff505f6a110ee71f00f314
kiricon <kiricon@live.com>
2016-09-10 16:55:26 -0600

    Created a static config class for handling the config file.

---
+++ js/helper/Config.js
@@ -0,0 +1,40 @@
+"use strict";
+/// <reference path="../../node.d.ts" />
+var process = require("process");
+var fs = require("fs");
+var Config = (function () {
+    function Config() {
+    }
+    //Check if config files exists
+    Config.exists = function () {
+        try {
+            return fs.statSync(Config.configPathFile).isFile();
+        }
+        catch (e) {
+            return false;
+        }
+    };
+    //Creates a new empty config file
+    Config.create = function () {
+        fs.writeFileSync(Config.configPathFile, "{}");
+        return Config.exists();
+    };
+    //gets top level value of config file
+    Config.get = function (item) {
+        return Config.object[item];
+    };
+    //Set's top level value of config file
+    Config.set = function (item, content) {
+        var configObject = Config.object();
+        configObject[item] = content;
+        fs.writeFileSync(Config.configPathFile, JSON.stringify(configObject));
+        return Config.exists();
+    };
+    //Gets the entire content of the config file as an object
+    Config.object = function () {
+        return JSON.parse(fs.readFileSync(Config.configPathFile, "utf8"));
+    };
+    Config.configPathFile = process.env.HOME + "/.sebastian/config.json";
+    return Config;
+}());
+exports.Config = Config;
---
+++ ts/helper/Config.ts
@@ -0,0 +1,44 @@
+/// <reference path="../../node.d.ts" />
+import * as process from "process";
+import * as fs from "fs";
+
+
+export class Config {
+    static configPathFile: string = process.env.HOME + "/.sebastian/config.json";
+
+    //Check if config files exists
+    static exists(): boolean {
+        try {
+            return fs.statSync(Config.configPathFile).isFile();
+        } catch (e) {
+            return false
+        }
+    }
+
+    //Creates a new empty config file
+    static create(): boolean {
+        fs.writeFileSync(Config.configPathFile, "{}");
+        return Config.exists();
+    }
+
+    //gets top level value of config file
+    static get(item: string): any {
+        return Config.object[item];
+    }
+
+    //Set's top level value of config file
+    static set(item: string, content: any): boolean {
+        let configObject = Config.object();
+        configObject[item] = content;
+
+        fs.writeFileSync(Config.configPathFile, JSON.stringify(configObject));
+
+        return Config.exists();
+    }
+
+    //Gets the entire content of the config file as an object
+    static object(): Object {
+        return JSON.parse(fs.readFileSync(Config.configPathFile, "utf8"));
+    }
+
+}
